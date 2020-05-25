9536155d335a8c64d8371a81358008434bddc028
samuelvontucher <work@samuelvontucher.com>
2020-05-25 17:28:08 +0200

    Fixed Problem when trying to connect to runway

--- app/v1/main.js
+++ app/v1/main.js
@@ -11,28 +11,50 @@ let fileInput;
 let img;
 
 function setup() {
-    canvas = createCanvas(512, 512);
+    canvas = createCanvas(800, 600);
     canvas.parent("canvas_container");
-    background(240, 200, 40);
+    background(255);
+    textAlign(CENTER);
+    imageMode(CENTER);
     setUpDOM();
     canvas.drop(handleFile);
 }
 
 function draw() {
+    background(255);
+    text("Drop Image File here", width/2, height/2);
     // draw loaded image on canvas
     if(img) {
-        image(img, 0, 0);
+        image(img, width/2, height/2);
+    }
+}
+
+
+function setUpDOM() {
+    statusText = document.querySelector("#image_status");
+    sendBtn = createButton("Send Image");
+    sendBtn.mousePressed(sendImage);
+    inputImage = document.querySelector("#input_image");
+}
+
+function handleFile(file) {
+    print(file);
+    statusText.innerHTML = "image loaded!";
+    if (file.type === 'image') {
+        img = loadImage(file.data);
+        inputImage.src = file.data;
+    } else {
+        img = null;
     }
 }
 
 //---
 // Function to access runway
 function sendImage() {
-    const canvasElt = canvas.elt;
-    const imageData = canvasElt.toDataURL("image/jpeg")
-
+    img.loadPixels();
+    postImage = img.canvas.toDataURL("image/jpeg");
     const inputs = {
-        "semantic_map": "<base 64 image>"
+        "semantic_map": postImage
     };
 
     fetch('http://localhost:8000/query', {
@@ -48,40 +70,22 @@ function sendImage() {
             const {
                 output
             } = outputs;
+            print("Data from Runway")
             print(outputs);
             generateImage(outputs);
             // use the outputs in your project
         });
 }
-
 //--
 
-
-function setUpDOM() {
-    statusText = document.querySelector("#image_status");
-    sendBtn = createButton("Send Image");
-    sendBtn.mousePressed(sendImage);
-    textAlign(CENTER);
-    text("Drop Image File here", width/2, height/2);
-}
-
-function handleFile(file) {
-    print(file);
-    statusText.innerHTML = "image loaded!";
-    if (file.type === 'image') {
-        img = loadImage(file.data);
-        inputImage = loadImage(file.data);
-        fileInput = createImg(file.data, '');
-        fileInput.style("width", "100px");
-        fileInput.style("height", "100px");
-        fileInput.parent("display_input");
-    } else {
-        inputImage = null;
-        img = null;
-    }
-}
-
 function generateImage(data) {
-    newImg = createImg(data.results);
-    image(0,0);
+    // create a new image based on the data from runway
+    if(data) {
+        print(data.output);
+        img = loadImage(data.output);
+        newImg = createImg(data.output, "Generated Image");
+    } else {
+        console.log("Error");
+    }
+    
 }
